c035390a70b99e38ab99401816b9176aa05203c2
Dineli <dinelikarunathilaka@gmail.com>
2016-03-04 13:59:33 +0800

    added log statements

--- MTC/src/main/java/com/nus/mtc/dao/impl/LocationDAOImpl.java
+++ MTC/src/main/java/com/nus/mtc/dao/impl/LocationDAOImpl.java
@@ -12,14 +12,17 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
- * @author EPHAADK
+ * @author Dineli
  */
 public class LocationDAOImpl implements ILocationDAO {
 
     private SessionFactory sessionFactory = null;
+    private static final Logger logger = LoggerFactory.getLogger(SampleDAOImpl.class);
 
     public LocationDAOImpl() {
         this.sessionFactory = HibernateUtil.getSessionFactory();
@@ -27,24 +30,40 @@ public class LocationDAOImpl implements ILocationDAO {
 
     @Override
     public List<Locations> fetchAllLocationData() {
+        logger.info("Fetching all location data");
         Session session = sessionFactory.openSession();
         StringBuilder sb = new StringBuilder();
+        List<Locations> countryList = null;
+
         sb.append("SELECT l FROM Locations l JOIN l.countryId c");
-        List<Locations> countryList = session.createQuery(sb.toString()).list();
-        session.close();
-        System.out.println("---------------------- Found " + countryList.size() + " countryList");
+        try {
+            countryList = session.createQuery(sb.toString()).list();
+            logger.info("Country List size " + countryList.size());
+        } catch (Exception exception) {
+            logger.error("Error occured while retirieving all location data");
+            logger.error(exception.getMessage());
+        } finally {
+            session.close();
+        }
         return countryList;
     }
 
     @Override
     public Locations fetchLocationDataById(int locationId) {
+        logger.info("Fetching all location data by ID");
         Session session = sessionFactory.openSession();
-
-        Query query = session.getNamedQuery("Locations.findById").setInteger("id", locationId);
-        Locations location = (Locations) query.uniqueResult();
-        session.close();
-        System.out.println("location =" + location.getCountryId().getName() + ", City="
-                + location.getCity());
+        Query query = null;
+        Locations location = null;
+        try {
+            query = session.getNamedQuery("Locations.findById").setInteger("id", locationId);
+            location = (Locations) query.uniqueResult();
+            logger.info("Country: " + location.getCountryId().getName());
+        } catch (Exception exception) {
+            logger.error("Error occured while retirieving all location data by ID");
+            logger.error(exception.getMessage());
+        } finally {
+            session.close();
+        }
         return location;
     }
 
